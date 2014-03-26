76ac9799a269b6b1d5ef1619c833778cf43fdd70
gedr <edgafarov@gmail.ru>
2014-03-26 09:11:34 +0400

    add entities for KADR's views

---
+++ CompetenceEntities/src/minos/entities/Division.java
@@ -0,0 +1,119 @@
+package minos.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import org.apache.openjpa.persistence.ReadOnly;
+import org.apache.openjpa.persistence.UpdateAction;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+
+/**
+ * The persistent class for the tOrgStru database table.
+ * 
+ */
+@Cacheable(true)
+@Entity
+@Table(name="tOrgStru")
+@NamedQuery(name="Division.findAll", query="SELECT d FROM Division d")
+public class Division implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="tOrgStruID")
+	private int id;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="BegDA")
+	private Timestamp beginDate;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="EndDA")
+	private Timestamp endDate;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="FullName")
+	private String fullName;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="SortOrder")
+	private int sortOrder;
+	
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="Isdelete")
+	private int isdelete;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="OtizOk")
+	private int otizOk;
+
+	// division's tree
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="Parent", referencedColumnName="tOrgStruID")    
+	private Division parentDivision; 
+
+	@OneToMany(mappedBy="parentDivision", fetch=FetchType.LAZY)
+	@OrderBy(value="name")
+	private List<Division> subDivisions;
+
+	public Division() { }
+
+	public int getId() {
+		return id;
+	}
+
+	public Timestamp getBeginDate() {
+		return this.beginDate;
+	}
+
+	public Timestamp getEndDate() {
+		return this.endDate;
+	}
+
+	public String getFullName() {
+		return this.fullName;
+	}
+
+	public int getIsdelete() {
+		return this.isdelete;
+	}
+
+	public int getOtizOk() {
+		return this.otizOk;
+	}
+
+	public int getSortOrder() {
+		return this.sortOrder;
+	}
+
+	public Division getParentDivision() {
+		return this.parentDivision;
+	}
+
+	public List<Division> getSubDivision() {
+		return this.subDivisions;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if(obj == null) return false;
+		if (!(obj instanceof Division)) return false;
+		if(this.id != ((Division) obj).id) return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Division: [" + String.valueOf(id) + "] " + fullName;
+	}
+}
---
+++ CompetenceEntities/src/minos/entities/Person.java
@@ -0,0 +1,137 @@
+package minos.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import org.apache.openjpa.persistence.ReadOnly;
+import org.apache.openjpa.persistence.UpdateAction;
+
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the tPersona database table.
+ * 
+ */
+@Cacheable(true)
+@Entity
+@Table(name="tPersona")
+@NamedQuery(name="Person.findAll", query="SELECT p FROM Person p")
+public class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="tPersonaId")
+	private int id;
+	
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="BegDA")
+	private Timestamp beginDate;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="EndDA")
+	private Timestamp endDate;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="Drojd")
+	private Timestamp birthdate;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="F")
+	private String surname;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="I")
+	private String name;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="O")
+	private String patronymic;
+
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="Sex")
+	private String sex;
+	
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="STAT2")
+	private int stat2;
+	
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@Column(name="Tab")
+	private String tab;
+
+/*
+	//bi-directional one-to-one association to PersonLogin
+	@ReadOnly(value=UpdateAction.IGNORE)
+	@OneToOne(mappedBy="person", fetch=FetchType.LAZY)
+	private PersonLogin personLogin;
+*/
+	public Person() { }
+
+	public int getId() {
+		return id;
+	}
+
+	public Timestamp getBeginDate() {
+		return this.beginDate;
+	}
+
+	public Timestamp getBirthdate() {
+		return this.birthdate;
+	}
+
+	public Timestamp getEndDate() {
+		return this.endDate;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getPatronymic() {
+		return this.patronymic;
+	}
+
+	public String getSex() {
+		return this.sex;
+	}
+
+	public int getStat2() {
+		return this.stat2;
+	}
+
+	public String getTab() {
+		return this.tab;
+	}
+
+	/*
+	public PersonLogin getPersonLogin() {
+		return this.personLogin;
+	}
+	*/
+    
+	@Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+    	if(this == obj) return true;
+        if(obj == null) return false;        
+        if (!(obj instanceof Person)) return false;
+        if(this.id != ((Person) obj).id) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: [" + String.valueOf(id) + " ] " + surname + " " + name + " " + patronymic;
+    }
+}
