8ad9ff64fc7f3f8c0f190b0e6b8a9a13bbe25caa
gedr <edgafarov@gmail.ru>
2014-03-27 11:17:35 +0400

    add variety const interface

---
+++ CompetenceEntities/src/minos/entities/VarietyConst.java
@@ -0,0 +1,9 @@
+package minos.entities;
+
+public interface VarietyConst {
+	public static final short VARIETY_COUNT 					= 3;
+	
+	public static final short PROFESSIONAL_COMPETENCE 			= 1;
+	public static final short PERSONALITY_BUSINESS_COMPETENCE 	= 2;
+	public static final short ADMINISTRATIVE_COMPETENCE			= 3;
+}
